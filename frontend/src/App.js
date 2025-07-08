08b2caebee7e7f2d305a757228de420d4e984d60
sli683 <sli683@aucklanduni.ac.nz>
2025-07-08 14:44:07 +1200

    Working minimal input and output features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,47 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
+
 function App() {
+
+  const [inputValue, setInputValue] = useState('');
+  const [output, setOutput] = useState([]);
+
+  const handleInput = (event) => {
+    if (event.key === 'Enter' && inputValue !== '') {
+      setOutput((prevOutputs) => [...prevOutputs, inputValue]);
+      fetch('http://localhost:5000/api/ask', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({ prompt : inputValue }),
+      })
+      .then(response => response.json())
+      .then(data => {
+        const rawData = data.message.content
+        setOutput((prevOutputs) => [...prevOutputs, rawData.split("</think>")[1]]);
+        console.log("Response from backend:", data);
+      })
+      .catch(error => {
+        console.error("Error calling backend:", error);
+      });
+      setInputValue('');
+    }
+  };
+
   return (
-    <div className="text-6xl">
-      Hello
+    <div>
+      <div className="h-[80vh] overflow-scroll">
+        {output.map((text, index) => (
+          index % 2 === 0 ? 
+            (<p key={index} className='text-left'>{text}</p>)
+            :
+            (<ReactMarkdown key={index}>{text}</ReactMarkdown>)
+          ))}
+      </div>
+      <input type="text" value={inputValue} onKeyDown={handleInput} onChange={(e) => setInputValue(e.target.value)}></input>
     </div>
   );
 }
